0de0b901768b51b511642ccef717861150a35faa
JABermudez11 <justinanbermudez@gmail.com>
2020-01-27 20:31:47 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  resHash = {}
+  
+    data.each do |att, hash|
+        hash.each do |trait, array|
+            array.each do |name|
+                if !resHash.has_key?(name)
+                    resHash[name] = {}
+                end
+
+                if !resHash[name].has_key?(att)
+                    resHash[name][att] = []
+                end
+
+                if !resHash[name][att].include?(att)
+                    resHash[name][att] << trait.to_s
+                end
+            end
+        end
+    end
+    resHash
 end
+
